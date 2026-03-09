c9f8e467604dd293261a5a28e99783c0a93529a2
Arif Yıldırım <61884097+Thunder-png@users.noreply.github.com>
2026-03-09 13:30:05 +0300

    Switch legal pages to custom page templates only

--- child-theme/page-gizlilik-politikasi.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Native WordPress page template by slug: gizlilik-politikasi.
- *
- * @package WooCommerce_Store_Child
- */
-
-require get_stylesheet_directory() . '/page-templates/privacy-policy.php';
